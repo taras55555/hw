7986dfef9d7c54bfa17742abe42e1111ce153cc4
taras55555 <tkaminskyi@gmail.com>
2023-05-20 12:42:31 +0300

    started

---
+++ 2023-05-19/hw.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/convert-a-string-to-an-array/train/javascript
+function stringToArray(string) {
+    return string.split(" ");
+}
+
+// https://www.codewars.com/kata/dna-to-rna-conversion/train/javascript
+DNAtoRNA = (dna) => dna.replaceAll("T", "U");
+
+//
